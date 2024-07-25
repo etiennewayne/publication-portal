38c2ac4d81193e39c1ecfd6adb14b4c802e99a2c
etiennewayne <etiennewayne@gmail.com>
2024-07-25 22:06:05 +0800

    article module started

--- resources/js/Pages/Admin/Article/ArticleCreateEdit.tsx
+++ resources/js/Pages/Admin/Article/ArticleCreateEdit.tsx
@@ -68,30 +68,73 @@ export default function ArticleCreateEdit({ id, auth }: {id:number, auth:PagePro
 	return (
 		<Authenticated user={auth.user}>
 
-			<Form layout='vertical'
-				form={form} autoComplete='off'>
-				
+			<div className='flex justify-center mt-6'>
 
-				<Form.Item
-					name="title"
-					label="Title"
-					validateStatus={errors.title ? 'error' : ''}
-					help={errors.title ? errors.title[0] : ''}
-					rules={[
-                        {
-                            required: true,
-                            message: 'Please input title',
-                        }
-                    ]}>
+				{/* card */}
+				<div className='w-full sm:w-[600px] bg-white p-6'>
 
-					<Input placeholder="Title"/>
+					<Form layout='vertical'
+						form={form} autoComplete='off'>
+						
 
-				</Form.Item>
+						<Form.Item
+							name="title"
+							label="Title"
+							validateStatus={errors.title ? 'error' : ''}
+							help={errors.title ? errors.title[0] : ''}
+							rules={[
+								{
+									required: true,
+									message: 'Please input title',
+								}
+							]}>
 
-				<Button htmlType='submit' type='primary'>
-					Save Article
-				</Button>
-			</Form>
+							<Input placeholder="Title"/>
+
+						</Form.Item>
+
+						<Form.Item
+							name="article_content"
+							label="Content"
+							validateStatus={errors.article_content ? 'error' : ''}
+							help={errors.article_content ? errors.article_content[0] : ''}
+							rules={[
+								{
+									required: true,
+									message: 'Please input content',
+								}
+							]}>
+
+							<Input.TextArea placeholder="Content"/>
+
+						</Form.Item>
+
+						
+						<Form.Item
+							name="author"
+							label="Author"
+							validateStatus={errors.author ? 'error' : ''}
+							help={errors.author ? errors.author[0] : ''}
+							rules={[
+								{
+									required: true,
+									message: 'Please input content',
+								}
+							]}>
+
+							<Input placeholder="Author"/>
+						</Form.Item>
+
+						<Button htmlType='submit' type='primary'>
+							Save Article
+						</Button>
+					</Form>
+				</div>
+				{/* end card */}
+			</div>
+			{/* card container */}
+
+
 
 		</Authenticated>
 	)
