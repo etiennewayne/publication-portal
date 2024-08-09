250434ab45cb8af8dc2b586ca49bef7159d8b941
etiennewayne <etiennewayne@gmail.com>
2024-08-09 12:54:38 +0800

    add any type in user

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -7,7 +7,7 @@ import { Link } from '@inertiajs/react';
 import { User } from '@/types';
 
 export default function Authenticated(
-    { user, header, children }: PropsWithChildren<{ user: User, header?: ReactNode }>) {
+    { user, header, children }: PropsWithChildren<{ user: any, header?: ReactNode }>) {
     
     
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -21,8 +21,6 @@ export default function Authenticated(
                             <div className="shrink-0 flex items-center">
                                 <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
                             </div>
-
-                          
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
