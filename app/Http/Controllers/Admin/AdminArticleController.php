4597fe99a751ef76943c83551869c848e5071807
etiennewayne <etiennewayne@gmail.com>
2024-08-22 05:54:40 +0800

    test

--- app/Http/Controllers/Admin/AdminArticleController.php
+++ app/Http/Controllers/Admin/AdminArticleController.php
@@ -154,6 +154,7 @@ class AdminArticleController extends Controller
         return Article::find($id);
         // comment zapanta gwapo
         //balos ko gwapo sad ko
+        //akoa ning update eshen
     }
 
     /**
